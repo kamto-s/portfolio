74d6345a3b22fa3ab8d274236688dfd30e5d139a
kamto-s <kamto.076s@gmail.com>
2025-03-19 01:57:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#10100f",
+        secondary: "#9e9e9e",
+      },
+    },
+  },
+  plugins: [],
+};
